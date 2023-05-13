2d2ce0706a7efa518629f8e3cbb376d0348f1541
Preetam Singh Dabi <preetamdabi1812@gmail.com>
2023-05-14 01:58:31 +0530

    Added get api call

--- routes/getRoutes.js
+++ routes/getRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = express.Router();
 const main = require('../scrapeFunc/scrape');
+const path = require('path');
+
 routes.post('/indeed', async(req, res) => {
     //whenever you use async, always use try catch
     try {
@@ -12,6 +14,16 @@ routes.post('/indeed', async(req, res) => {
         })
     } catch (e) {
         return res.status(500).send(e);
+    }
+})
+
+//router to getData
+routes.get('/getData',async(req,res)=>{
+    try {
+      const jobs =path.join(__dirname,'..','jobs.json')
+      res.sendFile(jobs);
+    } catch (error) {
+      return res.status(500).send(error);
     }
 })
 
